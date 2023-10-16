a45c9d83d134fecdf1502578a3a27572e0bb4a9f
KonorOko <125145871+KonorOko@users.noreply.github.com>
2023-10-16 03:34:58 +0000

    v1.0.1

--- Home.py
+++ Home.py
@@ -30,7 +30,7 @@ def run():
     st.markdown(
         """
         ### Página hecha para llevar control sobre algunos proyectos 
-        en conjunto con la preciosa más chinchuda de la FQ
+        ### en conjunto con la preciosa más chinchuda de la FQ
       """
     )
 
