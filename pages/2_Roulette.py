48914e111587a5dd191169e696e8ec6529d96c2f
KonorOko <125145871+KonorOko@users.noreply.github.com>
2023-10-17 06:29:21 -0600

    Update 2_Roulette.py

--- pages/2_Roulette.py
+++ pages/2_Roulette.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import random
+import time
 
+# Lista de opciones en la ruleta
+opciones = ["Opción 1", "Opción 2", "Opción 3", "Opción 4", "Opción 5"]
+
+# Botón para girar la ruleta
+if st.button("¡Girar la ruleta!"):
+    # Animación de giro
+    with st.spinner("Girando la ruleta..."):
+        time.sleep(2)  # Simula un giro de 2 segundos
+        opcion_seleccionada = random.choice(opciones)
+    st.success(f"¡La opción seleccionada es: {opcion_seleccionada}")
